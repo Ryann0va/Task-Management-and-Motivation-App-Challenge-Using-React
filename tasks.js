942e745a271f4e9341896f0e4f0e121a9703fbaa
Ryann0va <rhehn@usf.edu>
2024-07-12 17:59:15 -0400

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,10 @@
+const tasks = [
+    { id: 1, name: "Complete the React challenge", isCompleted: false },
+    { id: 2, name: "Read about React Hooks", isCompleted: false },
+    { id: 3, name: "Build a To-Do App", isCompleted: false },
+    { id: 4, name: "Write documentation", isCompleted: false },
+    { id: 5, name: "Test your application", isCompleted: false }
+  ];
+  
+  export default tasks;
+  
