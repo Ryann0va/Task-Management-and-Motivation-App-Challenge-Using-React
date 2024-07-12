7296527d186d872bba67f07f692c93407ca2fcbb
Ryann0va <rhehn@usf.edu>
2024-07-12 17:56:39 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FancyText from './FancyText';
+import TaskGenerator from './TaskGenerator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FancyText title="true" text="Task Management and Motivation App" />
+      <TaskGenerator />
+      <footer>
+        &copy; 2024 Your Name. All rights reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
